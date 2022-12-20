e2029eba7072217bfe88839de07fbe2f50239f3a
SirDom247 <dominicudousoro@gmail.com>
2022-12-19 16:40:26 -0800

    C pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,24 @@
 #include "main.h"
+
 /**
- * print_rev - prints a string, followed by a new line.
- * @s: input string to print.
+ * print_rev - a function that prints a string, in reverse
+ * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
  */
 void print_rev(char *s)
 {
-	int i;
+	int fcounter = 0;
+	int i, n;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		i++;
+		fcounter++;
 	}
-	i--;
-	while (s[i] != '\0')
+
+	for (n = (fcounter - 1); n >= 0; n--)
 	{
-		_putchar(s[i--]);
+		_putchar(s[n]);
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * puts2 - Prints out every other char of the string.
- * @str: input string to print.
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	while (str[i] != '\0')
+	for (; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
+		if ((i % 2) == 0)
 			_putchar(str[i]);
-		i++;
+		else
+			continue;
 	}
 	_putchar('\n');
 }
