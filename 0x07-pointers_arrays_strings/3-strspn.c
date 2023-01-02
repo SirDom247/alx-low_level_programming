e62a78c0f6b20639b899dd86a4b7a25f18093b5f
SirDom247 <dominicudousoro@gmail.com>
2023-01-02 14:47:46 -0800

    1. memcpy

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,7 +10,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i] = '\0')
 	{
 		j = 0;
 		while (accept[j] != '\0' && s[i] != accept[j])
